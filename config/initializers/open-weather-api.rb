6cc682333bd7d45c2e09433ccf4006da23002263
Kimberly Conkle <kimberlyconkle88@gmail.com>
2020-02-19 21:15:25 +0000

    lesson

--- config/initializers/open-weather-api.rb
+++
@@ -1,9 +0,0 @@
-OpenWeatherAPI.configure do |config|
-  # API key
-  config.api_key = "c305c02baa62ceae1f7a9c7937b7981a"
-
-  # Optionals
-  config.default_language = 'es'     # 'en' by default
-  config.default_country_code = 'es' # nil by default (ISO 3166-1 alfa2)
-  config.default_units = 'metric'    # 'metric' by default
-end
